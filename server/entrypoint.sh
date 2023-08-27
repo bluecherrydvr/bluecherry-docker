5fd6ed0165fa2a19e00ca2fced540885af49388e
Curtis Hall <curt@bluecherry.net>
2023-08-26 21:56:31 -0500

    Update entrypoint.sh

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -59,57 +59,16 @@ if [ $status -ne 0 ]; then
   exit $status
 fi
 
-entrypoint_log() {
-    if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
-        echo "$@"
-    fi
-}
-
-if [ "$1" = "nginx" ] || [ "$1" = "nginx-debug" ]; then
-    if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
-        entrypoint_log "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
-
-        entrypoint_log "$0: Looking for shell scripts in /docker-entrypoint.d/"
-        find "/docker-entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
-            case "$f" in
-                *.envsh)
-                    if [ -x "$f" ]; then
-                        entrypoint_log "$0: Sourcing $f";
-                        . "$f"
-                    else
-                        # warn on shell scripts without exec bit
-                        entrypoint_log "$0: Ignoring $f, not executable";
-                    fi
-                    ;;
-                *.sh)
-                    if [ -x "$f" ]; then
-                        entrypoint_log "$0: Launching $f";
-                        "$f"
-                    else
-                        # warn on shell scripts without exec bit
-                        entrypoint_log "$0: Ignoring $f, not executable";
-                    fi
-                    ;;
-                *) entrypoint_log "$0: Ignoring $f";;
-            esac
-        done
-
-        entrypoint_log "$0: Configuration complete; ready for start up"
-    else
-        entrypoint_log "$0: No files found in /docker-entrypoint.d/, skipping configuration"
-    fi
-fi
-
 exec "$@"
 
-#echo "> /usr/sbin/nginx"
+echo "> /usr/sbin/nginx"
 #source /etc/apache2/envvars
-#/usr/sbin/apache2
-#status=$?
-#if [ $status -ne 0 ]; then
-#  echo "Failed to start apache2 web server: $status"
-#  exit $status
-#fi
+/usr/sbin/nginx
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start nginx web server: $status"
+  exit $status
+fi
 
 
 echo "> /usr/sbin/bc-server -u bluecherry -g bluecherry"
