f975bcf9cfd4d4e319a5d3571775c2da2365ecc1
Andriy Utkin <andriy_utkin@fastmail.com>
2023-11-23 20:06:00 +0000

    scripts/install.sh: reindent

--- scripts/install.sh
+++ scripts/install.sh
@@ -27,67 +27,67 @@ check_docker_process() {
 
 
 docker_compose_init() {
-# Make sure we are still in the bluecherry-docker directory
+    # Make sure we are still in the bluecherry-docker directory
 
-#uptimekuma
+    #uptimekuma
 
 
-echo "Downloading latest Bluecherry and related images...this may take a while..."
+    echo "Downloading latest Bluecherry and related images...this may take a while..."
 
-cd "$workingpath/bluecherry-docker"
+    cd "$workingpath/bluecherry-docker"
 
-# Init the mailenv config
+    # Init the mailenv config
 
-cp $workingpath/bluecherry-docker/mailenv-example $workingpath/bluecherry-docker/.mailenv
+    cp $workingpath/bluecherry-docker/mailenv-example $workingpath/bluecherry-docker/.mailenv
 
-docker compose pull
-docker compose up bc-mysql -d
+    docker compose pull
+    docker compose up bc-mysql -d
 
-echo "Sleeping 45 seconds to make sure the database is initialized correctly..."
+    echo "Sleeping 45 seconds to make sure the database is initialized correctly..."
 
-sleep 45
-docker compose stop bc-mysql
-docker compose up -d bc-mysql
+    sleep 45
+    docker compose stop bc-mysql
+    docker compose up -d bc-mysql
 
-echo "Sleeping another 15 seconds to run the database creation scripts..."
-echo "\n\n"
+    echo "Sleeping another 15 seconds to run the database creation scripts..."
+    echo "\n\n"
 
-sleep 15
-docker compose run bluecherry bc-database-create
-docker compose down
-#for i in {1..50}; do
-#    if docker compose pull; then
-#        if docker compose up -d; then
-#            break
-#        fi
-#    fi
+    sleep 15
+    docker compose run bluecherry bc-database-create
+    docker compose down
+    #for i in {1..50}; do
+    #    if docker compose pull; then
+    #        if docker compose up -d; then
+    #            break
+    #        fi
+    #    fi
     sleep 10
-docker compose up -d
-#done
+    docker compose up -d
+    #done
 }
 
 
 configure_env() {
 
-echo "\n\n******************************************************************\n\n"
-echo "You will be asked the following to configure the docker container:
+    echo "\n\n******************************************************************\n\n"
+    echo "You will be asked the following to configure the docker container:
 
 Time Zone (formatted like this - See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
 Create a password for the mysql admin
 Create a password for the mysql bluecherry user
 "
 
-echo "Time Zone (i.e. - America/Chicago): "
-read timezoneset
+    echo "Time Zone (i.e. - America/Chicago): "
+    read timezoneset
 
-#read -p "Time Zone (i.e. - America/Chicago)：" timezone
-#timezoneset="${timezone:=American/Chicago}"
-read -p "Please provide a mysql admin password：" mysqladminpass
-read -p "Please provide a mysql bluecherry password：" mysqlbluecherrypass
+    #read -p "Time Zone (i.e. - America/Chicago)：" timezone
+    #timezoneset="${timezone:=American/Chicago}"
+    read -p "Please provide a mysql admin password：" mysqladminpass
+    read -p "Please provide a mysql bluecherry password：" mysqlbluecherrypass
 
 
-# Install variables
-echo "
+    # Install variables
+    echo "
 # Set to your desired timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 TZ=$timezoneset
 
@@ -130,16 +130,16 @@ BLUECHERRY_USER_ID=1000
 
 configure_smtp() {
 
-echo "Configure SMTP"
+    echo "Configure SMTP"
 
-read -p "Please provide the SMTP server: " smtpserver
-read -p "Please provide the SMTP username: " smtplogin
-read -p "Please provide the SMTP password: " smtppassword
-read -p "Please provide the SMTP port: " smtpport
-#read -p "Please provide the SMTP username: " smtplogin
+    read -p "Please provide the SMTP server: " smtpserver
+    read -p "Please provide the SMTP username: " smtplogin
+    read -p "Please provide the SMTP password: " smtppassword
+    read -p "Please provide the SMTP port: " smtpport
+    #read -p "Please provide the SMTP username: " smtplogin
 
 
-echo "
+    echo "
 
 SMTP_USERNAME=$smtplogin
 SMTP_PASSWORD=$smtppassword
@@ -153,52 +153,50 @@ ALWAYS_ADD_MISSING_HEADERS=yes
 }
 
 check_process() {
-  process_name=$1
-  status=$(docker ps --filter "name=$process_name" --format '{{.Names}}' | grep -w "$process_name")
+    process_name=$1
+    status=$(docker ps --filter "name=$process_name" --format '{{.Names}}' | grep -w "$process_name")
 
-  if [ -n "$status" ]; then
-    echo "\e[32m$process_name is running\e[0m"  # Green color for running process
-
-#exit 1
-  else
-    echo "\e[31m$process_name is not running\e[0m"  # Red color for not running process
-  fi
-#exit 1
-
+    if [ -n "$status" ]; then
+        echo "\e[32m$process_name is running\e[0m"  # Green color for running process
+        #exit 1
+    else
+        echo "\e[31m$process_name is not running\e[0m"  # Red color for not running process
+    fi
+    #exit 1
 }
 
 
 check_docker_processes() {
-  if docker ps --format '{{.Names}}' | grep -q -w 'bc-server' && docker ps --format '{{.Names}}' | grep -q -w 'bc-mysql'; then
-    echo "**********************************************************************"
-    echo
-    echo "Installation finished, you can access the Bluecherry server here below"
-    echo "The default login is Admin and the default password is bluecherry"
-    echo
-    echo "https://$IP:7001"
-    echo
-    echo "**********************************************************************"
-  else
-    echo -e "\n\n\e[31mWARNING: Either bc-server or bc-mysql process is not running in Docker.  Below is a listing of the current bc-server and bc-mysql processes\e[0m\n\n"
-    echo -e "If you are unable to resolve this issue please visit the Bluecherry community at https://forums.bluecherrydvr.com\n\n"
-    echo -e "Below are statuses of the 'bc-server' and 'bc-mysql' containers:\n\n"
-check_process "bc-server"
-check_process "bc-mysql"
-    echo -e "\n\n"
+    if docker ps --format '{{.Names}}' | grep -q -w 'bc-server' && docker ps --format '{{.Names}}' | grep -q -w 'bc-mysql'; then
+        echo "**********************************************************************"
+        echo
+        echo "Installation finished, you can access the Bluecherry server here below"
+        echo "The default login is Admin and the default password is bluecherry"
+        echo
+        echo "https://$IP:7001"
+        echo
+        echo "**********************************************************************"
+    else
+        echo -e "\n\n\e[31mWARNING: Either bc-server or bc-mysql process is not running in Docker.  Below is a listing of the current bc-server and bc-mysql processes\e[0m\n\n"
+        echo -e "If you are unable to resolve this issue please visit the Bluecherry community at https://forums.bluecherrydvr.com\n\n"
+        echo -e "Below are statuses of the 'bc-server' and 'bc-mysql' containers:\n\n"
+        check_process "bc-server"
+        check_process "bc-mysql"
+        echo -e "\n\n"
 
-  fi
+    fi
 }
 
 
 
 configure_nfs() {
 
-echo "installing nfs"
+    echo "installing nfs"
 
-read -p "Please provide the IP address of the NFS server: " nfsserver
-read -p "Please provide the NFS server export path: " nfsexport
+    read -p "Please provide the IP address of the NFS server: " nfsserver
+    read -p "Please provide the NFS server export path: " nfsexport
 
-echo "
+    echo "
 
 version: '3.8'
 services:
@@ -221,27 +219,27 @@ volumes:
 
 clone_bluecherrydocker() {
 
- local directory=$pwd/bluecherry-docker
+    local directory=$pwd/bluecherry-docker
     if [ -d "$directory" ]; then
         echo "Directory $directory exists...skipping...."
-     return 0
-else
+        return 0
+    else
 
-echo "Directory $directory...proceeding with cloning"
+        echo "Directory $directory...proceeding with cloning"
 
-cd "$workingpath"
-#mkdir bluecherry-server
-#cd bluecherry-server
-git clone https://github.com/bluecherrydvr/bluecherry-docker.git
+        cd "$workingpath"
+        #mkdir bluecherry-server
+        #cd bluecherry-server
+        git clone https://github.com/bluecherrydvr/bluecherry-docker.git
 
-# Sigh...Fedora LXC containers and maybe others set max keys at 2000.  We need more.  For now more systems are needed to test on.
-# https://unix.stackexchange.com/questions/600968/disk-quota-exceeded-when-trying-to-deploy-docker-container-inside-lxc
-#echo 5000 | tee /proc/sys/kernel/keys/maxkeys
-#echo "kernel.keys.maxkeys=5000" >  /etc/sysctl.d/99-custom.conf
+        # Sigh...Fedora LXC containers and maybe others set max keys at 2000.  We need more.  For now more systems are needed to test on.
+        # https://unix.stackexchange.com/questions/600968/disk-quota-exceeded-when-trying-to-deploy-docker-container-inside-lxc
+        #echo 5000 | tee /proc/sys/kernel/keys/maxkeys
+        #echo "kernel.keys.maxkeys=5000" >  /etc/sysctl.d/99-custom.conf
 
 
-#return 1
-fi
+        #return 1
+    fi
 
 }
 
@@ -249,86 +247,86 @@ fi
 
 install_bluecherry() {
 
-echo -e "\n\nInstalling Bluecherry.......\n\n"
+    echo -e "\n\nInstalling Bluecherry.......\n\n"
 
-distribution=$(detect_distribution)
+    distribution=$(detect_distribution)
 
-case $distribution in
-  "debian" | "ubuntu")
-    install_debian_packages
-    ;;
-  "centos" | "rhel" | "fedora" | "rocky" | "Rocky")
-    install_redhat_packages
-    ;;
-  "sles" | "opensuse" | "suse")
-    install_suse_packages
-    ;;
-  "arch")
-    install_arch_packages
-    ;;
-  "fedora")
-    install_fedora_packages
-    ;;
-  # Add cases for other distributions and call the appropriate installation functions
-  *)
-    echo "Unsupported distribution, please contact Bluecherry for support in adding your distribution.  Please provide output of /etc/os-release and /etc/lsb_release"
-    exit 1
-    ;;
-esac
+    case $distribution in
+        "debian" | "ubuntu")
+            install_debian_packages
+            ;;
+        "centos" | "rhel" | "fedora" | "rocky" | "Rocky")
+            install_redhat_packages
+            ;;
+        "sles" | "opensuse" | "suse")
+            install_suse_packages
+            ;;
+        "arch")
+            install_arch_packages
+            ;;
+        "fedora")
+            install_fedora_packages
+            ;;
+            # Add cases for other distributions and call the appropriate installation functions
+        *)
+            echo "Unsupported distribution, please contact Bluecherry for support in adding your distribution.  Please provide output of /etc/os-release and /etc/lsb_release"
+            exit 1
+            ;;
+    esac
 }
 # Function to detect the Linux distribution
 detect_distribution() {
-  if [ -f "/etc/os-release" ]; then
-    . /etc/os-release
-    echo "$ID"
-  else
-    echo "Unsupported distribution"
-    exit 1
-  fi
+    if [ -f "/etc/os-release" ]; then
+        . /etc/os-release
+        echo "$ID"
+    else
+        echo "Unsupported distribution"
+        exit 1
+    fi
 }
 
 # Function to install packages on Debian-based distributions
 install_debian_packages() {
-  apt-get update
-  apt-get install -y git
+    apt-get update
+    apt-get install -y git
 }
 
 # Function to install packages on Red Hat-based distributions
 install_redhat_packages() {
-  yum update
-  yum install -y git
-  install_docker
-# Fix mainly for rocky...but we will try on all RHEL releases
-  sed -i 's/^LimitNOFILE=infinity$/LimitNOFILE=1048576/'  /usr/lib/systemd/system/docker.service
-  sed -i 's/^LimitNOFILE=infinity$/LimitNOFILE=1048576/'  /usr/lib/systemd/system/containerd.service
-  systemctl daemon-reload
-  systemctl stop docker
-  systemctl start docker
+    yum update
+    yum install -y git
+    install_docker
+    # Fix mainly for rocky...but we will try on all RHEL releases
+    sed -i 's/^LimitNOFILE=infinity$/LimitNOFILE=1048576/'  /usr/lib/systemd/system/docker.service
+    sed -i 's/^LimitNOFILE=infinity$/LimitNOFILE=1048576/'  /usr/lib/systemd/system/containerd.service
+    systemctl daemon-reload
+    systemctl stop docker
+    systemctl start docker
 }
 
 # Function to install packages on SUSE-based distributions
 install_suse_packages() {
-  zypper refresh
-  zypper install -y git
-  install_docker
+    zypper refresh
+    zypper install -y git
+    install_docker
 }
 
 # Function to install packages on Arch Linux
 install_arch_packages() {
-  pacman -Syu --noconfirm git
-  install_docker
-  sed -i 's/^LimitNOFILE=infinity$/LimitNOFILE=1048576/'  /usr/lib/systemd/system/docker.service
-  sed -i 's/^LimitNOFILE=infinity$/LimitNOFILE=1048576/'  /usr/lib/systemd/system/containerd.service
-  systemctl start docker
-  systemctl enable docker
-  pacman -Syu --noconfirm docker-compose
+    pacman -Syu --noconfirm git
+    install_docker
+    sed -i 's/^LimitNOFILE=infinity$/LimitNOFILE=1048576/'  /usr/lib/systemd/system/docker.service
+    sed -i 's/^LimitNOFILE=infinity$/LimitNOFILE=1048576/'  /usr/lib/systemd/system/containerd.service
+    systemctl start docker
+    systemctl enable docker
+    pacman -Syu --noconfirm docker-compose
 }
 
 # Function to install packages on Fedora
 install_fedora_packages() {
-  dnf update
-  dnf install -y git
-  install_docker
+    dnf update
+    dnf install -y git
+    install_docker
 }
 
 install_docker() {
@@ -336,36 +334,31 @@ install_docker() {
     if command -v docker > /dev/null 2>&1; then
         echo -e "\n\nDocker is installed....skipping!\n\n"
         return 0
-
     else
-
-
- curl -fsSL https://get.docker.com -o /tmp/install-docker.sh
-  sh /tmp/install-docker.sh
-  systemctl start docker
-  systemctl enable docker
+        curl -fsSL https://get.docker.com -o /tmp/install-docker.sh
+        sh /tmp/install-docker.sh
+        systemctl start docker
+        systemctl enable docker
     fi
 
 }
 
 uptimekuma() {
 
-echo -e "Installing Uptime Kuma for monitoring of Bluecherry services\n\n"
+    echo -e "Installing Uptime Kuma for monitoring of Bluecherry services\n\n"
 
-	DOWNLOAD_URL='https://github.com/louislam/uptime-kuma/releases/download/1.21.3/dist.tar.gz'
+    DOWNLOAD_URL='https://github.com/louislam/uptime-kuma/releases/download/1.21.3/dist.tar.gz'
 
-	cd "$workingpath/bluecherry-docker" || exit 1
+    cd "$workingpath/bluecherry-docker" || exit 1
 
-		wget "${DOWNLOAD_URL}"
-		tar -zxf "$workingpath/dist.tar.gz" -C "$workingpath"
+    wget "${DOWNLOAD_URL}"
+    tar -zxf "$workingpath/dist.tar.gz" -C "$workingpath"
 
+    echo -e "Installing Uptime Kuma for monitoring of Bluecherry services\n\n"
 
-
-echo -e "Installing Uptime Kuma for monitoring of Bluecherry services\n\n"
-
-cd bluecherry-docker
-wget https://github.com/louislam/uptime-kuma/releases/download/1.21.3/dist.tar.gz
-tar -xvf dist.tar.gz
+    cd bluecherry-docker
+    wget https://github.com/louislam/uptime-kuma/releases/download/1.21.3/dist.tar.gz
+    tar -xvf dist.tar.gz
 }
 
 read -p "Do you want to install docker and setup Bluecherry server? [y/n]: " answer
@@ -373,10 +366,10 @@ read -p "Do you want to install docker and setup Bluecherry server? [y/n]: " ans
 # Execute the appropriate function based on the answer
 case $answer in
     y)
-       install_docker
-       install_bluecherry
-#	   configure_env
-       #clone_bluecherrydocker
+        install_docker
+        install_bluecherry
+        # configure_env
+        #clone_bluecherrydocker
         ;;
     n)
         ;;
@@ -389,12 +382,12 @@ read -p "Do you want to download and configure the Bluecherry docker images?  If
 
 case $clonedocker in
     y)
-clone_bluecherrydocker
-configure_env
-docker_compose_init
+        clone_bluecherrydocker
+        configure_env
+        docker_compose_init
         ;;
     n)
-#        exit
+        # exit
         ;;
     *)
         echo "Invalid answer"
@@ -408,7 +401,7 @@ case $smtp in
         configure_smtp
         ;;
     n)
-#        exit
+        # exit
         ;;
     *)
         echo "Invalid answer"
@@ -422,9 +415,9 @@ read -p "Do you want to add a NFS mount? [y/n]: " add_nfs
 case $add_nfs in
     y)
         configure_nfs
-;;
+        ;;
     n)
-#        exit
+        # exit
         ;;
     *)
         echo "Invalid answer"
